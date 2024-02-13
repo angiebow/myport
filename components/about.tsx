b0091ac01ee5693c0aebaf825c62addc900e3d94
Pelangi Masita <121101474+angiebow@users.noreply.github.com>
2024-02-13 11:54:58 +0700

    Update about.tsx

--- components/about.tsx
+++ components/about.tsx
@@ -21,15 +21,6 @@ export default function About() {
       <p className="mb-3">
         About me.
       </p>
-
-      <p>
-        <span className="italic">When I'm not coding</span>, I enjoy playing
-        video games, watching movies, and playing with my dog. I also enjoy{" "}
-        <span className="font-medium">learning new things</span>. I am currently
-        learning about{" "}
-        <span className="font-medium">history and philosophy</span>. I'm also
-        learning how to play the guitar.
-      </p>
     </motion.section>
   );
 }
